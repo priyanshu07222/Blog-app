0406cce3f7ad45e0aa686238fe16f3f883cb9ed2
Priyanshu <priyanshu16181389@gmail.com>
2024-05-05 15:40:09 +0530

    reinitialized client and setup router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 
-function App() {
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import { Home, About, Dashboard, Signin, Signup, Projects } from './pages/index.js'
 
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout/>} >
+        <Route path='' element={<Home/>} />
+        <Route path='projects' element={<Projects/>} />
+        <Route path='sign-up' element={<Signup/>} />
+        <Route path='sign-in' element={<Signin/>} />
+        <Route path='about' element={<About/>} />
+        <Route path='dashboard' element={<Dashboard/>} />
+      </Route>
+    )
+  )
   return (
-    <>
-      <h1>Hello priyansu</h1>
-    </>
+    <div className="bg-gray-700 text-white">
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home.jsx";
+import About from "./About.jsx";
+import Dashboard from "./Dashboard.jsx";
+import Projects from "./Projects.jsx";
+import Signin from "./Signin.jsx";
+import Signup from "./Signup.jsx";
+
+export {Home, About, Dashboard, Projects, Signin, Signup }
