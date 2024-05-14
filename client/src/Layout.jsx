0b01b4f0297466b4cfa0140e6a414a2998d43420
Priyanshu <priyanshu16181389@gmail.com>
2024-05-14 22:09:47 +0530

    create the footer components

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
-import Footer from './components/Footer'
+import FooterCom from './components/Footer'
 
 function Layout() {
   return (
     <div>
         <Header/>
         <Outlet/>
-        <Footer/>
+        <FooterCom/>
     </div>
   )
 }
